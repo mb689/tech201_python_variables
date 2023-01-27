81af1dfaef9da730013f61f82a5c14d3b0cefd50
Mohamed Boudjatat <mboudjatat17@hotmail.com>
2023-01-27 15:55:12 +0000

    Added a program which asks for name, age and DOB

---
+++ variables.py
@@ -0,0 +1,22 @@
+# Variables
+# variable_name = variable_data
+
+a = 1
+b = 2
+c = 3.5
+hi = "Hello World"
+d = True
+
+# print(hi)
+# print(a)
+
+# print(type(d))
+
+name = input("What is your name? ")
+age = input("What is you age? ")
+DOB = input("What is you DOB? ")
+
+print(name)
+print(age)
+print(DOB)
+
